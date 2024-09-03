6dc4a56fe66e7fcd0ec756195b5c81486194f6f4
Z3_N1X <iliya.souri7890@gmail.com>
2024-09-03 18:43:54 +0330

    Start webview and start server on startup

---
+++ main.py
@@ -0,0 +1,8 @@
+import webview
+
+def custom_logic(window):
+    window.toggle_fullscreen()
+    window.evaluate_js('alert("Nice one brother")')
+
+window = webview.create_window('Woah dude!', html='<h1>Woah dude!<h1>')
+webview.start(custom_logic, window)
