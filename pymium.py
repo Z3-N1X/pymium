59a487d14a3030b6a83a7c4a02ea8b6d7aebde83
Z3_N1X <iliya.souri7890@gmail.com>
2024-09-04 17:14:13 +0330

    added more args to run

--- pymium.py
+++ pymium.py
@@ -4,6 +4,6 @@ from core.base import Types
 from core.element import Element, handler
 from core import Space
 
-def run(space):
-    window = webview.create_window('Transparency Test', html=str(space), transparent=True, frameless=True, width=300, height=150, on_top=True)
+def run(space:Space, width: int = 800, height: int = 600, always_on_top: bool = False, transparent: bool = False, frameless: bool= False):
+    window = webview.create_window(space.title, html=str(space), transparent=transparent, frameless=frameless, width=width, height=height, on_top=always_on_top)
     webview.start()
