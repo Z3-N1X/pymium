854ad4e0460d3d2b58d18e0c7df6c9bb4f015377
Z3N <59246899+Z3-N1X@users.noreply.github.com>
2024-09-04 19:01:25 +0330

    Create setup.py

---
+++ pymium/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='pymium',
+version='0.1.0',
+author='Z3N',
+author_email='iliya.souri7890@example.com',
+description='Windows application creator',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.11',
+install_requires=["pywebview"]
+)
